92a3f46beed4d0a53ce00fb72dfd5839265edab5
whizdor <118841840+whizdor@users.noreply.github.com>
2023-10-12 01:59:00 +0530

    add file.psd

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route export path = "/" element = {<Home />} />
+        <Route export path = "/gdsc" element = {<Home />} />
         <Route export path = "/projects" element = {<Project />} />
       </Routes>
     </div>
